ae6e2374e5de36ea75eb207f824941fec5bb9bd8
Abl1405 <fancyfire.yash@gmail.com>
2017-09-26 11:16:17 +0530

    Lab Assignment 7

---
+++ src/jUnitTestExample/testAddNumbers.java
@@ -0,0 +1,16 @@
+package jUnitTestExample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddNumbers {
+
+	@Test
+	public void test() {
+		jUnitTesting jUnitInt = new jUnitTesting();
+		int result = jUnitInt.addNumbers(50, 75);
+		assertEquals(125, result);
+	}
+
+}
